896325e1f4f4f4815ecca2236dd2f41b20b05a2e
rushi-smb <rushi@smbios.co.in>
2023-10-20 16:49:50 +0530

    Add List<string> of icons for label

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,28 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        //  Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        /*  Each of these letters is an interesting icon
+            in the Webdings font,
+            and each icon appears twice in this list    */
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
     }
 }
